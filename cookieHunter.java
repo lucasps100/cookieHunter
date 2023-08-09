0eb58e91eaf5ab9650985f5970e82d2e1433c171
Luke Shepherd <sheppyluke7@gmail.com>
2023-08-09 17:07:53 -0400

    Finish cookie hunter.

--- cookieHunter.java
+++ cookieHunter.java
@@ -1,18 +1,23 @@
+package cookies.cookieHunter;
+
 import java.util.Scanner;
 import java.util.Random;
 
-public class cookiesLab {
+public class cookieHunter {
 
     public static void main(String[] args) {
         String[] rooms = {"Office", "Living Room", "Kitchen", "Garage", "Bathroom"};
         Scanner console = new Scanner(System.in);
         Random random = new Random();
         int cookieRoomNumber = random.nextInt(1,rooms.length - 1);
+        System.out.println(cookieRoomNumber);
         String cookieRoom = rooms[cookieRoomNumber];
         boolean hasCookie = false;
         boolean leftOffice = false;
         boolean exit = false;
         String currentRoom = "Office";
+        System.out.println("Welcome to Cookie Hunter!\n" +
+                "Find the cookie and get back to the office.\n");
 
         while(!exit) {
 
@@ -44,17 +49,53 @@ public class cookiesLab {
                             "4. Go to Garage.\n" +
                             "5. Go to Bathroom");
                     currentRoom = rooms[Integer.parseInt(console.nextLine()) - 1];
-                    if (currentRoom.equals("Living Room")){
+                    if (currentRoom.equals("Living Room")) {
+                        if (cookieRoomNumber == 1) {
+                            hasCookie = true;
+                            System.out.println("Congrats! Your found the cookie!");
+                        } else {
+                            System.out.println("No cookie!");
+                        }
+                    }
+                        break;
+                case "Kitchen":
+                    System.out.println("1. Go back to Office.\n" +
+                            "2. Go to Living Room.\n" +
+                            "3. Look around.\n" +
+                            "4. Go to Garage.\n" +
+                            "5. Go to Bathroom");
+                    currentRoom = rooms[Integer.parseInt(console.nextLine()) - 1];
+                    if (currentRoom.equals("Kitchen")){
                         if(cookieRoomNumber == 2) {
                             hasCookie = true;
                             System.out.println("Congrats! Your found the cookie!");
                         } else {
                             System.out.println("No cookie!");
                         }
-                    } else {
-                        break;
                     }
-                case "Kitchen":
+                        break;
+
+                case "Garage":
+                    System.out.println("1. Go back to Office.\n" +
+                            "2. Go to Living Room.\n" +
+                            "3. Go to Kitchen.\n" +
+                            "4. Look around.\n" +
+                            "5. Go to Bathroom");
+                    currentRoom = rooms[Integer.parseInt(console.nextLine()) - 1];
+                    if (currentRoom.equals("Garage")){
+                        if(cookieRoomNumber == 3) {
+                            hasCookie = true;
+                            System.out.println("Congrats! Your found the cookie!");
+                        } else {
+                            System.out.println("No cookie!");
+                        }
+                    }
+                        break;
+
+                case "Bathroom":
+                    System.out.println("Don't eat bathroom cookies!");
+                    currentRoom = "Office";
+                    break;
             }
         }
 
